51e1369362cf50e2f0e578e0b7949b63599de9c0
👋 Hi, I'm Suraj Vishwakarma <itxsurajofficial@gmail.com>
2026-02-12 17:45:56 +0530

    Refactor App component with updated Router structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,29 @@ import Banner from "./Components/Banner";
 import About from "./Components/About";
 import Skills from "./Components/Skills";
 import Expr from "./Components/Expr.jsx";
-import Graduation from "./Components/Graducation.jsx"; // Consider renaming the file to Graduation.jsx
+import Graduation from "./Components/Graducation.jsx";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 
 function App() {
   return (
- <Router>
-  <Header />
-
-  {/* Main content with top padding */}
-  <div className="pt-20 md:pt-20"> {/* Adjust 24,28 to match header height */}
-    <Routes>
-      <Route path="/" element={<Banner />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/skills" element={<Skills />} />
-      <Route path="/education" element={<Graduation />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/expr" element={<Expr />} />
-    </Routes>
-  </div>
-
-  <Footer />
-</Router>
-
+    <Router basename="/React-portfolio3/"> 
+      
+      <Header />
+      <div className="pt-20 md:pt-20">
+        <Routes>
+          <Route path="/" element={<Banner />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/education" element={<Graduation />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/expr" element={<Expr />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
